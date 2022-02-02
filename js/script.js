79b68710afeb4055b930ef04d5eac6a37b6ee6c5
Glitch (buttered-interesting-nurse) <none>
2022-02-02 23:25:29 +0000

    😅💬 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,4 +1,28 @@
 // Complete Day 1 goals here
+let MJ = ["Beat It","Micheal Jackson","https://i3.ytimg.com/vi/WlTlUseVt7E/maxresdefault.jpg","https://www.youtube.com/watch?v=WlTlUseVt7E"]
+let MJ2 = ["Billie Jean","Micheal Jackson","https://i3.ytimg.com/vi/OZGtRvYF-A4/maxresdefault.jpg","https://www.youtube.com/watch?v=OZGtRvYF-A4"]
+let MJ3 = ["Thriller","Micheal Jackson","https://i3.ytimg.com/vi/BsuEjAw5hg0/maxresdefault.jpg","https://www.youtube.com/watch?v=BsuEjAw5hg0"]
+
+// strip all this down and convert to Song object 
+// Future Songs to add:
+/*
+
+QUEEN
+Radio Ga Ga 
+Bohemian Rhapsody
+Don't Stop Me Now
+
+George Micheal
+Last Christmas
+Careless Whisper
+
+Tears For Fear
+Everbyody wants to Rule The Woorld
+
+
+*/
+
+
 
 function displaySongInfo() {
   // Complete the Day 2 goals inside this function
